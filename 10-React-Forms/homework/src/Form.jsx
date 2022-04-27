e062ff10bd167b92dbf7462b382cf864dc7b3d6e
Exivac <exivacz@gmail.com>
2022-04-27 11:43:08 -0300

    Pending for completion

--- 10-React-Forms/homework/src/Form.jsx
+++ 10-React-Forms/homework/src/Form.jsx
@@ -1,9 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { validate } from './validator';
+
 
 export default function  Form() {
+  //Internal Functions
+
+  const [input, setInput] = React.useState({
+    username: '',
+    password: '',
+  })
+
+  const [errors, setErrors] = React.useState({});
+
+  const handleInputChange = function(e) {
+    setInput({
+      ...input,
+      [e.target.name]: e.target.value
+    });
+  }
+
+
+
+  //Return Value
   return (
-      <div>
-        Componente Form
-      </div>
+      <form>
+        <div>
+          <label htmlFor="Usermame">Username:</label>
+          <input 
+          type="text" 
+          name='username' 
+          value={input['username']}
+          onChange={handleInputChange}
+          />
+
+          <label htmlFor="Password">Password:</label>
+          <input 
+          type="password" 
+          name="password" 
+          value={input['password']}
+          onChange={handleInputChange}
+          />
+          
+          <input type="submit" name='Submit' onSubmit={(e) => e.preventDefault}/>
+        </div>
+      </form>
   )
 }
